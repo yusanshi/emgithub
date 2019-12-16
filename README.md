482979d207d649bf973a7dbc8bb260578aef926a
yusanshi <yulei2018@gmail.com>
2019-12-16 10:33:52 +0800

    Finish skeleton

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# embed_like_gist
+# Embed Like Gist
+
+Embed file on Github just like [gist](https://gist.github.com/).
+
+- [ ] Show spinner while loading
+- [ ] Support more style from  [GitHub syntax theme generators](https://github.com/primer/github-syntax-theme-generator)
+- [ ] Check input
+
+## Get started
+
+There are two ways to use the service.
+
+Supposed you want to embed this README file whose URL is `https://github.com/yusanshi/embed_like_gist/blob/master/README.md`. The first way is to visit https://emgithub.com/ and paste the URL. The other is to simply add "em" before "github.com". For this README file, you edit URL into `https://emgithub.com/yusanshi/embed_like_gist/blob/master/README.md`, then press Enter.
+
+
+## Examples
+
+https://blog.yusanshi.com/2019-12-17-emgithub/#%E6%B5%8B%E8%AF%95
+
+## Credits
+- CSS from [GitHub syntax theme generators](https://github.com/primer/github-syntax-theme-generator);
+- Icons made by [Vectors Market](https://www.flaticon.com/authors/vectors-market) from [www.flaticon.com](https://www.flaticon.com/).
---
+++ embed.js
@@ -0,0 +1,57 @@
+embed();
+
+function embed() {
+  var url = new URL(document.currentScript.src);
+  var target = new URL(url.searchParams.get("target"));
+  var pathSplit = target.pathname.split("/");
+  var user = pathSplit[1];
+  var repository = pathSplit[2];
+  var branch = pathSplit[4];
+  var file = pathSplit.slice(5, pathSplit.length).join("/");
+  var rawFile = `https://raw.githubusercontent.com/${user}/${repository}/${branch}/${file}`;
+
+  document.write(`<div class="${pathSplit.join("-")}"></div>`);
+
+  fetch(rawFile).then(function (response) {
+    return response.text()
+  }).then(function (text) {
+    var allDiv = document.getElementsByClassName(pathSplit.join("-"));
+    for (var i = 0; i < allDiv.length; i++) {
+      // Avoid duplicate when the same code is embeded more than once
+      if (!allDiv[i].hasChildNodes()) {
+        allDiv[i].innerHTML = `
+        <link rel="stylesheet" href="https://github.githubassets.com/assets/gist-embed-123720f37c57ce9a8f29de081c38ed61.css">
+        <div class="gist">
+          <div class="gist-file">
+            <div class="gist-data">
+              <div class="js-gist-file-update-container js-task-list-container file-box">
+                <div class="file">
+                  <div itemprop="text" class="Box-body p-0 blob-wrapper data">
+                    <table class="highlight tab-size js-file-line-container" data-tab-size="8">
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="gist-meta">
+              <a target="_blank" href="${rawFile}" style="float:right">view raw</a>
+              <a target="_blank" href="${target.href}">${pathSplit[pathSplit.length - 1]}</a>
+              hosted with &#10084; by <a target="_blank"
+                href="${window.location.protocol}//${window.location.host}">EmGithub.com</a>
+            </div>
+          </div>
+        </div>
+        `;
+
+        allDiv[i].getElementsByClassName("js-file-line-container")[0].appendChild(document.createTextNode(text));
+      }
+    };
+  }).catch(function (error) {
+    var allDiv = document.getElementsByClassName(filter(rawFile));
+    for (var i = 0; i < allDiv.length; i++) {
+      if (!allDiv[i].hasChildNodes()) {
+        allDiv[i].appendChild(document.createTextNode(`Failed to fetch ${rawFile}: ${error.message}`));
+      }
+    };
+  });
+}
