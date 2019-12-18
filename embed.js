19d54b48f919d15ee4872cd9885550d822ce4966
yusanshi <yulei2018@gmail.com>
2019-12-18 22:47:57 +0800

    Hide validation icon for small device

--- embed.js
+++ embed.js
@@ -11,8 +11,8 @@ function embed() {
   var file = pathSplit.slice(5, pathSplit.length).join("/");
   var rawFile = `https://raw.githubusercontent.com/${user}/${repository}/${branch}/${file}`;
 
-  // Add Highlight.js, CSS for spinner, and reserve space for codeArea
-  // reserving space for codeArea should be done earlier than loading of Highlight.js
+  // Add Highlight.js, CSS for spinner, and reserve space for code area
+  // reserving space for code area should be done earlier than loading of Highlight.js
   // To avoid `Found 0 div` problem.
   document.write(`
   <style>.lds-ring{margin:0 auto;position:relative;width:60px;height:60px}.lds-ring div{box-sizing:border-box;display:block;position:absolute;width:48px;height:48px;margin:6px;border:6px solid #fff;border-radius:50%;animation:lds-ring 1.2s cubic-bezier(0.5,0,0.5,1) infinite;border-color:#888 transparent transparent transparent}.lds-ring div:nth-child(1){animation-delay:-.45s}.lds-ring div:nth-child(2){animation-delay:-.3s}.lds-ring div:nth-child(3){animation-delay:-.15s}@keyframes lds-ring{0%{transform:rotate(0deg)}100%{transform:rotate(360deg)}}</style>
