4bfe61183ca7b113a74bf183e6d2beba8c9e0fc8
yusanshi <yulei2018@gmail.com>
2020-01-16 01:34:45 +0800

    Finish part of line number

--- embed.js
+++ embed.js
@@ -22,7 +22,9 @@ function embed() {
   document.write(`
   <style>.lds-ring{margin:1rem auto;position:relative;width:60px;height:60px}.lds-ring div{box-sizing:border-box;display:block;position:absolute;width:48px;height:48px;margin:6px;border:6px solid #fff;border-radius:50%;animation:lds-ring 1.2s cubic-bezier(0.5,0,0.5,1) infinite;border-color:#888 transparent transparent transparent}.lds-ring div:nth-child(1){animation-delay:-.45s}.lds-ring div:nth-child(2){animation-delay:-.3s}.lds-ring div:nth-child(3){animation-delay:-.15s}@keyframes lds-ring{0%{transform:rotate(0deg)}100%{transform:rotate(360deg)}}</style>
   <div class="${className}"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>
+  <style>.hljs-ln-numbers{-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;text-align:right;color:#ccc;vertical-align:top}.hljs-ln td.hljs-ln-numbers{padding-right:1.25rem}</style>
   <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/highlight.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/highlightjs-line-numbers.js@2.7.0/dist/highlightjs-line-numbers.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/styles/${style}.min.css">
   <link rel="stylesheet" href="https://emgithub.com/main.css">
   `);
@@ -37,8 +39,9 @@ function embed() {
     const allDiv = document.getElementsByClassName(className);
     for (let i = 0; i < allDiv.length; i++) {
       if (allDiv[i].getElementsByClassName("lds-ring").length) {
-        if (typeof hljs == "undefined") {
-          console.log("Trying to reload highlight.js");
+        if (typeof hljs == "undefined" || typeof hljs.highlightBlock == "undefined" || typeof hljs.lineNumbersBlock == "undefined") {
+          //TODO
+          console.log("Trying to reload highlight");
           const hljsScript = document.createElement("script");
           hljsScript.onload = function () {
             console.log("Succeeded reloading highlight.js");
@@ -86,14 +89,14 @@ function embedCodeToTarget(targetDiv, codeText, showBorder, showLineNumbers, sho
   }
   code.textContent = codeText;
   hljs.highlightBlock(code);
+  if (showLineNumbers) {
+    hljs.lineNumbersBlock(code);
+  }
   pre.appendChild(code);
   const fileContainer = document.createElement("div");
   const fileBody = document.createElement("div");
   const fileMeta = document.createElement("div");
   fileBody.classList.add("file-body");
-  if (showLineNumbers) {
-    //TODO fileBody and pre
-  }
   if (showFileMeta) {
     const fileURLSplit = fileURL.split("/");
     fileMeta.innerHTML = `<a target="_blank" href="${rawFileURL}" style="float:right">view raw</a>
